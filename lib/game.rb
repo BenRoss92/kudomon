01c0d1770f30dc42ad4ceb09235945d26b8b6fb5
BenRoss92 <benjaminjackross@gmail.com>
2016-12-04 13:35:51 +0000

    Add required files to Game

--- lib/game.rb
+++ lib/game.rb
@@ -1,4 +1,9 @@
 require './lib/kudomon_types/chikapu'
+require './lib/kudomon_types/geoflude'
+require './lib/kudomon_types/mancharred'
+require './lib/kudomon_types/mewclue'
+require './lib/kudomon_types/sourbulb'
+require './lib/kudomon_types/squirkle'
 
 class Game
 
